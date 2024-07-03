cde5c3304c3d1f7df06171b9c82d195b52d1d427
Fabián Andrés Henry Vilaxa <113631396+Fhv75@users.noreply.github.com>
2024-07-03 14:12:47 -0400

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,144 @@
 import { useEffect, useState } from 'react';
 import NavBar from './NavBar/NavBar';
-import { Box, Button, Center, Checkbox, Flex, Heading, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, SimpleGrid, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Checkbox, Flex, Heading, Input, SimpleGrid, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text, useToast } from '@chakra-ui/react';
 import axios from 'axios';
 
-const SERVER_URL = 'http://18.229.124.1:3000';
+const SERVER_URL = 'http://localhost:3000';
 
 function App() {
   const [state, setState] = useState('');
   const [humedad, setHumedad] = useState(0);
   const [temperatura, setTemperatura] = useState(0);
+  const [riegoActivo, setRiegoActivo] = useState(false);
+  const [startTime, setStartTime] = useState("00:00");
+  const [isHumidityEnabled, setIsHumidityEnabled] = useState(false);
+  const [sliderValue, setSliderValue] = useState(60);
+  const toast = useToast();
 
   useEffect(() => {
-    axios.get(SERVER_URL + '/getCommandIT')
-      .then((response) => {
-        setState(response.data);
-      })
-      .catch((error) => {
+    const fetchCommandAndProgramacion = async () => {
+      try {
+        const commandResponse = await axios.get(SERVER_URL + '/getCommandIT');
+        setState(commandResponse.data);
+        
+        const programacionResponse = await axios.get(SERVER_URL + '/getProgramacionRiegoIT');
+        setIsHumidityEnabled(programacionResponse.data.humedad !== -1);
+        setSliderValue(programacionResponse.data.humedad);
+        setStartTime(programacionResponse.data.horaInicio);
+      } catch (error) {
         console.error('Error fetching data: ', error);
-      });
-  }, [state]);
+        toast({
+          title: 'Error al obtener datos.',
+          description: 'No se pudieron obtener los datos del servidor.',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    };
 
-  useEffect(() => {
-    axios.get(SERVER_URL + '/getProgramacionRiego')
-      .then((response) => {
-        setIsHumidityEnabled(response.data.humedad !== -1);
-        setSliderValue(response.data.humedad);
-        setStartTime(response.data.horaInicio);
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-      });
-  }, [state]);
+    fetchCommandAndProgramacion();
 
-  useEffect(() => {
     const interval = setInterval(() => {
       axios.get(SERVER_URL + '/getDatos')
         .then((response) => {
           setHumedad(response.data.humedad);
           setTemperatura(response.data.temperatura);
+          setRiegoActivo(response.data.riegoActivo); // Actualizar el estado del riego
+          setState(
+            response.data.riegoActivo ? 'START' : 'STOP'
+          ); // Actualizar el estado localmente
         })
         .catch((error) => {
           console.error('Error fetching humedad: ', error);
+          toast({
+            title: 'Error al obtener datos.',
+            description: 'No se pudieron obtener los datos de humedad.',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          });
         });
     }, 2000);
 
     return () => clearInterval(interval);
-  }, []);
-
-  const [startTime, setStartTime] = useState("00:00");
-  const [endTime, setEndTime] = useState("00:00");
-
-  const [isHumidityEnabled, setIsHumidityEnabled] = useState(false);
-
-  const [sliderValue, setSliderValue] = useState(60);
+  }, [toast]);
 
   const handleSetProgramacionRiego = () => {
-    // Si algun campo esta deshabilitado, no se envia
-
     let data = {
       humedad: isHumidityEnabled ? sliderValue : -1,
       startTime: isHumidityEnabled ? startTime : -1,
     };
 
-    console.log(data);
-
     axios.post(SERVER_URL + '/setProgramacionRiego', data)
       .then(response => {
         console.log('Success:', response.data);
+        toast({
+          title: 'Programación guardada.',
+          description: 'La programación del riego ha sido guardada exitosamente.',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
       })
       .catch((error) => {
         console.error('Error:', error);
+        toast({
+          title: 'Error al guardar programación.',
+          description: 'No se pudo guardar la programación del riego.',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
       });
-  }; 
+  };
+
+  const handleToggleCommand = () => {
+    const newCommand = state === 'START' ? 'STOP' : 'START';
+    axios.post(SERVER_URL + '/setCommand', { command: newCommand })
+      .then((response) => {
+        setState(newCommand); // Actualizar el estado localmente
+
+        // Resetear la programación localmente
+        setIsHumidityEnabled(false);
+        setSliderValue(60);
+        setStartTime("00:00");
+
+        toast({
+          title: `Riego ${newCommand === 'START' ? 'iniciado' : 'detenido'}.`,
+          description: `El riego ha sido ${newCommand === 'START' ? 'iniciado' : 'detenido'} manualmente.`,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        toast({
+          title: 'Error al cambiar estado.',
+          description: 'No se pudo cambiar el estado del riego.',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+      });
+  };
 
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar />
       <SimpleGrid columns={2} spacing={10} mt={20} mx={20}>
         <Box border={
           state === 'START' ? '2px solid green' : '2px solid orange'
-        } p={10} borderRadius={10} boxShadow="lg"
-        >
-          <Heading >Programa el Riego</Heading>
+        } p={10} borderRadius={10} boxShadow="lg">
+          <Heading>Programa el Riego</Heading>
           <Box mt={8}>
             <Flex align="center" mt={4}>
               <Heading size="lg" color="cyan.500">Humedad:</Heading>
-              <Checkbox colorScheme="cyan" defaultIsChecked={isHumidityEnabled} ml={2} onChange={(e) => setIsHumidityEnabled(e.target.checked)}></Checkbox>
+              <Checkbox colorScheme="cyan" isChecked={isHumidityEnabled} ml={2} onChange={(e) => setIsHumidityEnabled(e.target.checked)}></Checkbox>
             </Flex>
             <Box>
-              <Slider min={0} max={100} step={1} isDisabled={!isHumidityEnabled} defaultValue={60} onChange={(val) => setSliderValue(val)}>
+              <Slider min={0} max={100} step={1} isDisabled={!isHumidityEnabled} value={sliderValue} onChange={(val) => setSliderValue(val)}>
                 <SliderTrack bg={
                   sliderValue < 55 ? 'green.100' : sliderValue < 75 ? 'yellow.100' : 'red.100'
                 }>
@@ -100,7 +150,6 @@ function App() {
               </Slider>
               <Text>Valor actual: {sliderValue}%</Text>
             </Box>
-
             <Flex align="center" mt={4}>
               <Heading size="lg" color="cyan.500">Duración:</Heading>
             </Flex>
@@ -108,10 +157,9 @@ function App() {
               type="time" isDisabled={!isHumidityEnabled} 
               onChange={(e) => setStartTime(e.target.value)}
               step="1"
+              value={startTime}
             />
-
             <Button mt={8} colorScheme="green" onClick={handleSetProgramacionRiego}>Guardar</Button>
-
           </Box>
         </Box>
         <Box border={
@@ -119,27 +167,12 @@ function App() {
         } p={10} borderRadius={10} boxShadow="lg">
           <Center flexDir={"column"}>
             <Heading size="xl" color="cyan.500" mb={4}>Estado del Riego:</Heading>
-            {
-              state === 'START' ? <Heading size="lg" color="green.500" py={4}>ENCENDIDO</Heading> : <Heading size="lg" color="orange.500" my={4}>APAGADO</Heading>
-            }
-            <Button my={4} w={100} onClick={() => {
-              axios.post(SERVER_URL + '/setCommand', {
-                command: state === 'START' ? 'STOP' : 'START'
-              })
-                .then((response) => {
-                  setState(response.data);
-                })
-                .catch((error) => {
-                  console.error('Error fetching data: ', error);
-                });
-            }}
+            {riegoActivo ? <Heading size="lg" color="green.500" py={4}>ENCENDIDO</Heading> : <Heading size="lg" color="orange.500" my={4}>APAGADO</Heading>}
+            <Button my={4} w={100} onClick={handleToggleCommand}
               colorScheme={
-                state === 'START' ? 'orange' : 'green'
-              }
-            >
-              {
-                state === 'START' ? 'Apagar' : 'Encender'
-              }
+                riegoActivo ? 'orange' : 'green'
+              }>
+              {riegoActivo ? 'Apagar' : 'Encender'}
             </Button>
             <Center mt={8}>
               <Heading size="xl" color="cyan.500" mr={6}>Humedad:</Heading>
